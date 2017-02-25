b43fd52da40cd8255f6cff841a812c7d1f203d67
nsingla <nitinkumar.singla@macquarie.com>
2017-02-25 15:22:05 +0530

    Refactored code

--- src/main/java/com/example/controller/HelloWorldConroller.java
+++ src/main/java/com/example/controller/HelloWorldConroller.java
@@ -1,4 +1,4 @@
-package com.example.spring.boot.controller;
+package com.example.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +12,11 @@ public class HelloWorldConroller {
 
     @RequestMapping("/")
     public String index() {
-        return "index";
+        return "Welcome to sample web application based on spring web mvc";
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "hello";
+        return "Hi! Welcome to sample web application based on spring web mvc";
     }
 }
--- src/main/java/com/example/main/SampleWebApplication.java
+++ src/main/java/com/example/main/SampleWebApplication.java
@@ -1,14 +1,15 @@
-package com.example.spring.boot.main;
-import com.example.spring.boot.controller.HelloWorldConroller;
+package com.example.main;
+
+import com.example.controller.HelloWorldConroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = {HelloWorldConroller.class})
-public class SpringBootSampleWebApplication {
+public class SampleWebApplication {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(SpringBootSampleWebApplication.class, args);
+        SpringApplication.run(SampleWebApplication.class, args);
     }
 }
