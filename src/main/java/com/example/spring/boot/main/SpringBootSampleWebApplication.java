a353dd4f348537a564af64d308f29b4104781f7b
nsingla <nitinkumar.singla@macquarie.com>
2017-02-25 13:52:23 +0530

    Added support for static contect

--- src/main/java/com/example/spring/boot/main/SpringBootSampleWebApplication.java
+++ src/main/java/com/example/spring/boot/main/SpringBootSampleWebApplication.java
@@ -1,8 +1,11 @@
 package com.example.spring.boot.main;
+import com.example.spring.boot.controller.HelloWorldConroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {HelloWorldConroller.class})
 public class SpringBootSampleWebApplication {
 
     public static void main(String[] args) throws Exception {
